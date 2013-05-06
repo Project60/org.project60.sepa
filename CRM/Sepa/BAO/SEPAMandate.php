5b1cbe1e8aeef2694094035e0a7abf894f0707ee
Xavier Dutoit <xavier@tttp.eu>
2013-05-06 16:08:07 +0200

    generates a mandate reference if not provided

--- CRM/Sepa/BAO/SEPAMandate.php
+++ CRM/Sepa/BAO/SEPAMandate.php
@@ -4,6 +4,10 @@
  */
 class CRM_Sepa_BAO_SEPAMandate extends CRM_Sepa_DAO_SEPAMandate {
 
+  // TODO: generate a more meaningful reference?
+  function generateReference () {
+    return md5(uniqid(rand(), TRUE));
+  }
 
   /**
    * @param array  $params         (reference ) an assoc array of name/value pairs
@@ -13,6 +17,10 @@ class CRM_Sepa_BAO_SEPAMandate extends CRM_Sepa_DAO_SEPAMandate {
    * @static (I do appologize, I don't want to)
    */
   static function add(&$params) {
+    if (!CRM_Utils_Array::value('reference', $params)) {
+      $params["reference"] = CRM_Sepa_BAO_SEPAMandate::generateReference();
+    }
+ 
     $hook = empty($params['id']) ? 'create' : 'edit';
     CRM_Utils_Hook::pre($hook, 'SepaMandate', CRM_Utils_Array::value('id', $params), $params);
 
@@ -24,8 +32,7 @@ class CRM_Sepa_BAO_SEPAMandate extends CRM_Sepa_DAO_SEPAMandate {
       return civicrm_api3_create_error($e->getMessage());
     }
     CRM_Utils_Hook::post($hook, 'SepaMandate', $dao->id, $dao);
-    return $this;
+    return $dao;
   }
-
 }
 
