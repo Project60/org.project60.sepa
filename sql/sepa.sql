af3a1e8bab723cce92db530eab6d31b3c8f5a208
Xavier Dutoit <xavier@tttp.eu>
2013-05-10 14:04:53 +0200

    regenerated the sql too

--- sql/sepa.sql
+++ sql/sepa.sql
@@ -1,32 +1,28 @@
 
 -- DROP TABLE IF EXISTS 'civicrm_sdd_creditor';
 -- DROP TABLE IF EXISTS `civicrm_sdd_mandate`;
-
 -- /*******************************************************
 -- *
--- * civicrm_sdd_mandate
+-- * civicrm_sdd_creditor
 -- *
 -- *******************************************************/
 CREATE TABLE IF NOT EXISTS `civicrm_sdd_creditor`(
 
 
      `id` int unsigned NOT NULL AUTO_INCREMENT  COMMENT 'ID',
-     `reference` varchar(35) NOT NULL   COMMENT 'The unique mandate reference',
-     `source` varchar(64)    COMMENT 'Needed or coming from ContributionRecur? phoning/online/face 2 face....',
-     `iban` varchar(42) NULL   ,
-     `contact_id` int unsigned    COMMENT 'FK to Contact ID that owns that account',
-     `status_id` int unsigned NOT NULL  DEFAULT 3 COMMENT 'pseudo FK into civicrm_option_value.',
-     `BIX` varchar(64)    COMMENT '  ' 
+     `creditor_id` int unsigned    COMMENT 'FK to Contact ID that owns that account',
+     `identifier` varchar(35)    COMMENT 'Provided by the bank. ISO country code+check digit+ZZZ+country specific identifier',
+     `name` varchar(255)    COMMENT 'by default creditor_id.display_name snapshot at creation',
+     `address` varchar(255)    COMMENT 'by default creditor_id.address (billing) at creation',
+     `country_id` int unsigned    COMMENT 'Which Country does this address belong to.',
+     `mandate_prefix` varchar(3)    COMMENT 'prefix for mandate identifiers',
+     `category` varchar(4)    COMMENT 'Default value' 
 ,
     PRIMARY KEY ( `id` )
  
-    ,     UNIQUE INDEX `iban`(
-        iban
-  )
-  
-,          CONSTRAINT FK_civicrm_sdd_creditor_contact_id FOREIGN KEY (`contact_id`) REFERENCES `civicrm_contact`(`id`) ON DELETE SET NULL  
+ 
+,          CONSTRAINT FK_civicrm_sdd_creditor_creditor_id FOREIGN KEY (`creditor_id`) REFERENCES `civicrm_contact`(`id`) ON DELETE SET NULL,          CONSTRAINT FK_civicrm_sdd_creditor_country_id FOREIGN KEY (`country_id`) REFERENCES `civicrm_country`(`id`) ON DELETE SET NULL  
 )  ENGINE=InnoDB DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci  ;
-
 
 -- /*******************************************************
 -- *
@@ -34,17 +30,17 @@ CREATE TABLE IF NOT EXISTS `civicrm_sdd_creditor`(
 -- *
 -- *******************************************************/
 CREATE TABLE IF NOT EXISTS `civicrm_sdd_mandate` (
-
-
      `id` int unsigned NOT NULL AUTO_INCREMENT  COMMENT 'ID',
      `reference` varchar(35) NOT NULL   COMMENT 'The unique mandate reference',
      `source` varchar(64)    COMMENT 'Needed or coming from ContributionRecur? phoning/online/face 2 face....',
-     `contribution_recur_id` int unsigned    COMMENT 'FK to Contact Recuring Contribution associated with the mandate',
+     `entity_table` varchar(64)    COMMENT 'physical tablename for entity being joined, eg contributionRecur or Membership',
+     `entity_id` int unsigned NOT NULL   COMMENT 'FK to entity table specified in entity_table column.',
+     `date` datetime NOT NULL  DEFAULT now() COMMENT 'by default now()',
      `creditor_id` int unsigned    COMMENT 'FK to ssd_creditor',
      `contact_id` int unsigned    COMMENT 'FK to Contact ID that owns that account',
      `iban` varchar(42) NULL   COMMENT 'Iban of the debtor',
-     `bic` varchar(11) NULL   COMMENT 'BIC of the debtor',
-     `type` varchar(1) NOT NULL  DEFAULT "R" COMMENT 'R for recurrent (default) O for one-shot',
+     `bic` varchar(11)    COMMENT 'BIC of the debtor',
+     `type` varchar(1) NOT NULL  DEFAULT R COMMENT 'R for recurrent (default) O for one-shot',
      `enabled_id` tinyint NOT NULL  DEFAULT 1 COMMENT 'If the mandate has been validated',
      `creation_date` datetime    ,
      `validation_date` datetime     
@@ -53,12 +49,4 @@ CREATE TABLE IF NOT EXISTS `civicrm_sdd_mandate` (
  
     ,     UNIQUE INDEX `reference`(
         reference
-  )
-  ,     UNIQUE INDEX `iban`(
-        iban
-  )
-  
-,          CONSTRAINT FK_civicrm_sdd_mandate_contribution_recur_id FOREIGN KEY (`contribution_recur_id`) REFERENCES `civicrm_contribution_recur`(`id`) ON DELETE SET NULL,          CONSTRAINT FK_civicrm_sdd_mandate_contact_id FOREIGN KEY (`contact_id`) REFERENCES `civicrm_contact`(`id`) ON DELETE SET NULL  
-)  ENGINE=InnoDB DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci  ;
-
 
