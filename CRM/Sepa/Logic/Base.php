e68b7a2e0dda3e015d850e05b910d0380d227b37
Xavier Dutoit <xavier@tttp.eu>
2013-06-27 18:33:09 +0200

    no more fatal if not SDD

--- CRM/Sepa/Logic/Base.php
+++ CRM/Sepa/Logic/Base.php
@@ -11,13 +11,13 @@ class CRM_Sepa_Logic_Base {
     // get contrib page info
     $a = civicrm_api('ContributionPage', 'getsingle', array('version' => 3, 'id' => $contrib->contribution_page_id));
     if (!array_key_exists("payment_processor", $a)) {
-      CRM_Core_Error::fatal(ts("Can't find contribution page for this contribution"));
+      return false; //payment recorded from the backoffice probably
     }
 
     // get pp info
     $a = civicrm_api('PaymentProcessor', 'getsingle', array('version' => 3, 'id' => $a['payment_processor']));
     if (!array_key_exists("class_name", $a)) {
-      CRM_Core_Error::fatal(ts("Can't find payment processor for this contribution page"));
+      return false; // not sure what happened, but at least we know it's not a SDD
     }
 
     if ($a['class_name'] == 'Payment_SEPA_DD')
