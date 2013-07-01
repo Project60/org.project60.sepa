8f7d75c45a0514cf51d9d91ba38adcf3963d023b
Paul Delbar <paul.delbar@delius.be>
2013-07-01 21:33:05 +0200

    reordered the sql statements

--- sql/sepa.sql
+++ sql/sepa.sql
@@ -96,47 +96,6 @@ CREATE TABLE IF NOT EXISTS `civicrm_sdd_mandate` (
 )  ENGINE=InnoDB DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci  ;
 
 
-CREATE TABLE `civicrm_sdd_txgroup` (
-
-
-     `id` int unsigned NOT NULL AUTO_INCREMENT  COMMENT 'ID',
-     `reference` varchar(64)    COMMENT 'End-to-end reference for this tx group.',
-     `type` char(4)    COMMENT 'FRST, RCUR or OOFF',
-     `collection_date` datetime    COMMENT 'Target collection date',
-     `latest_submission_date` datetime    COMMENT 'Latest submission date',
-     `created_date` datetime    COMMENT 'When was this item created',
-     `status_id` int unsigned NOT NULL   COMMENT 'fk to Batch Status options in civicrm_option_values',
-     `sdd_creditor_id` int unsigned    COMMENT 'fk to SDD Creditor Id',
-     `sdd_file_id` int unsigned    COMMENT 'fk to SDD File Id'
-,
-    PRIMARY KEY ( `id` )
-
-
-,          CONSTRAINT FK_civicrm_sdd_txgroup_sdd_creditor_id FOREIGN KEY (`sdd_creditor_id`) REFERENCES `civicrm_sdd_creditor`(`id`) ON DELETE SET NULL,          CONSTRAINT FK_civicrm_sdd_txgroup_sdd_file_id FOREIGN KEY (`sdd_file_id`) REFERENCES `civicrm_sdd_file`(`id`) ON DELETE SET NULL
-)  ENGINE=InnoDB DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci  ;
-
-
-
-CREATE TABLE `civicrm_sdd_contribution_txgroup` (
-
-
-     `id` int unsigned NOT NULL AUTO_INCREMENT  COMMENT 'primary key',
-     `contribution_id` int unsigned NOT NULL   COMMENT 'FK to Contribution ID',
-     `txgroup_id` int unsigned NOT NULL   COMMENT 'FK to civicrm_sdd_txgroup'
-,
-    PRIMARY KEY ( `id` )
-
-    ,     UNIQUE INDEX `contriblookup`(
-        contribution_id
-  )
-  ,     UNIQUE INDEX `txglookup`(
-        txgroup_id
-  )
-
-,          CONSTRAINT FK_civicrm_sdd_contribution_txgroup_txgroup_id FOREIGN KEY (`txgroup_id`) REFERENCES `civicrm_sdd_txgroup`(`id`) ON DELETE SET NULL
-)  ENGINE=InnoDB DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci  ;
-
-
 -- /*******************************************************
 -- *
 -- * civicrm_sdd_file
@@ -159,5 +118,51 @@ CREATE TABLE `civicrm_sdd_file` (
 
 ,          CONSTRAINT FK_civicrm_sdd_file_created_id FOREIGN KEY (`created_id`) REFERENCES `civicrm_contact`(`id`) ON DELETE SET NULL
 )  ENGINE=InnoDB DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci  ;
+
+
+
+
+
+CREATE TABLE `civicrm_sdd_txgroup` (
+
+
+     `id` int unsigned NOT NULL AUTO_INCREMENT  COMMENT 'ID',
+     `reference` varchar(64)    COMMENT 'End-to-end reference for this tx group.',
+     `type` char(4)    COMMENT 'FRST, RCUR or OOFF',
+     `collection_date` datetime    COMMENT 'Target collection date',
+     `latest_submission_date` datetime    COMMENT 'Latest submission date',
+     `created_date` datetime    COMMENT 'When was this item created',
+     `status_id` int unsigned NOT NULL   COMMENT 'fk to Batch Status options in civicrm_option_values',
+     `sdd_creditor_id` int unsigned    COMMENT 'fk to SDD Creditor Id',
+     `sdd_file_id` int unsigned    COMMENT 'fk to SDD File Id'
+,
+    PRIMARY KEY ( `id` )
+
+
+,          CONSTRAINT FK_civicrm_sdd_txgroup_sdd_creditor_id FOREIGN KEY (`sdd_creditor_id`) REFERENCES `civicrm_sdd_creditor`(`id`) ON DELETE SET NULL,
+          CONSTRAINT FK_civicrm_sdd_txgroup_sdd_file_id FOREIGN KEY (`sdd_file_id`) REFERENCES `civicrm_sdd_file`(`id`) ON DELETE SET NULL
+)  ENGINE=InnoDB DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci  ;
+
+
+
+CREATE TABLE `civicrm_sdd_contribution_txgroup` (
+
+
+     `id` int unsigned NOT NULL AUTO_INCREMENT  COMMENT 'primary key',
+     `contribution_id` int unsigned NOT NULL   COMMENT 'FK to Contribution ID',
+     `txgroup_id` int unsigned NOT NULL   COMMENT 'FK to civicrm_sdd_txgroup'
+,
+    PRIMARY KEY ( `id` )
+
+    ,     UNIQUE INDEX `contriblookup`(
+        contribution_id
+  )
+  ,     UNIQUE INDEX `txglookup`(
+        txgroup_id
+  )
+
+,          CONSTRAINT FK_civicrm_sdd_cGGoup_id FOREIGN KEY (`txgroup_id`) REFERENCES `civicrm_sdd_txgroup`(`id`) ON DELETE SET NULL
+)  ENGINE=InnoDB DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci  ;
+
 
 
