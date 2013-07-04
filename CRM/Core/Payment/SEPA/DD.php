607d7eca75fe33ee162c057551e660f0a49f07c5
Xavier Dutoit <xavier@tttp.eu>
2013-07-04 23:46:52 +0200

    send pdf

--- CRM/Core/Payment/SEPA/DD.php
+++ CRM/Core/Payment/SEPA/DD.php
@@ -79,6 +79,9 @@ class CRM_Core_Payment_SEPA_DD extends CRM_Core_Payment {
     if ($r["is_error"]) {
       CRM_Core_Error::fatal( 'Mandate creation failed : ' . $r["error_message"]);
     }
+    
+    $page = CRM_Sepa_Page_SepaMandatePdf();
+    $page->generatePDF (true);
   }
 
   
