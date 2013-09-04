595f51b785e258ef097ea2486e22bfa6e4c6fd76
Your Name <you@example.com>
2013-09-04 18:04:35 +0200

    working on the recurring creation

--- api/v3/SepaTransactionGroup.php
+++ api/v3/SepaTransactionGroup.php
@@ -104,7 +104,7 @@ function civicrm_api3_sepa_transaction_group_getdetail($params) {
   $group = (int) $params["id"];
   if (!$group)
     throw new API_Exception("Incorrect or missing value for group id");
-  $sql = "select contribution_id, contrib.contact_id, contrib.financial_type_id, contrib.payment_instrument_id, total_amount, receipt_date, mandate.reference, mandate.validation_date FROM civicrm_sdd_contribution_txgroup, civicrm_contribution as contrib, civicrm_contribution_recur as recur, civicrm_sdd_mandate as mandate where mandate.entity_id= recur.id and contribution_id = contrib.id and contribution_recur_id=recur.id AND mandate.is_enabled=1 AND txgroup_id=$group";
+  $sql = "select contribution_id, contrib.contact_id, contrib.financial_type_id, contrib.payment_instrument_id, total_amount, receipt_date, mandate.reference, mandate.validation_date, recur.id as recur_id, recur.frequency_unit, recur.cycle_day FROM civicrm_sdd_contribution_txgroup, civicrm_contribution as contrib, civicrm_contribution_recur as recur, civicrm_sdd_mandate as mandate where mandate.entity_id= recur.id and contribution_id = contrib.id and contribution_recur_id=recur.id AND mandate.is_enabled=1 AND txgroup_id=$group";
   $dao = CRM_Core_DAO::executeQuery($sql);
   $result= array();
   $total =0;
@@ -113,5 +113,22 @@ function civicrm_api3_sepa_transaction_group_getdetail($params) {
     $total += $dao->total_amount;
   }
   return civicrm_api3_create_success($result, $params, NULL, NULL, $dao, $extraReturnValues = array("total_amount"=>$total));
+}
+
+function civicrm_api3_sepa_transaction_group_createnext_spec (&$params) {
+  $params['id']['api.required'] = 1;
+die ("toto");
+}
+
+function civicrm_api3_sepa_transaction_group_createnext ($params) {
+  $group = (int) $params["id"];
+  if (!$group)
+    throw new API_Exception("Incorrect or missing value for group id");
+  $contribs = civicrm_api("sepa_transaction_group","getdetail", $params);
+  foreach ($contribs["values"] as $contrib) {
+print_r($contrib);
+    $m = date_format($contrib["receipt_date"], "m");
+die ("$m");
+  }
 }
 
--- templates/Sepa/Page/DashBoard.tpl
+++ templates/Sepa/Page/DashBoard.tpl
@@ -2,10 +2,18 @@
 <h1>{$file.reference}</h1>
 {assign var='key' value="api.SepaTransactionGroup.get"}
 {assign var='groups' value=$file.$key.values}
-<hr>
 {foreach from=$groups item=group}
   {crmAPI var='result' entity='SepaTransactionGroup' action='getdetail' sequential=1 id=$group.id}
-<li><h3>{$group.latest_submission_date}:{$group.reference} <i>{$result.count} transactions for {$result.total_amount}€</i></h3>
+<div class="status_{$result.status_id}">
+<h3>{$group.reference}</h3>
+<ul>
+<li>Type: {$group.type}</li>
+<li>Creation: {$group.created_date}</li>
+<li>Collection: {$group.collection_date}</li>
+<li>Transactions: {$result.count}</li>
+<li>Total: {$result.total_amount} &euro;</li>
+<li><a href="#" class="button">Close & Generate next batch</a></li>
+</ul>
   <table>
   {foreach from=$result.values item=tx}
 {assign var="reference" value=$tx.reference}
@@ -14,6 +22,7 @@
   </table>
 </li>
 {/foreach}
+</div>
 {/foreach}
 
 
