0c62a4f75e2594b8ad607eb0798ba4225b184e0a
nikobo <bochan@systopia.de>
2014-05-21 09:53:52 +0200

    add headers

--- tests/phpunit/CRM/sepa/BatchingTest.php
+++ tests/phpunit/CRM/sepa/BatchingTest.php
@@ -1,4 +1,17 @@
 <?php
+/*-------------------------------------------------------+
+| Project 60 - SEPA direct debit                         |
+| Copyright (C) 2013-2014 TTTP                           |
+| Author: X+                                             |
++--------------------------------------------------------+
+| This program is released as free software under the    |
+| Affero GPL license. You can redistribute it and/or     |
+| modify it under the terms of this license which you    |
+| can read by viewing the included agpl.txt or online    |
+| at www.gnu.org/licenses/agpl.html. Removal of this     |
+| copyright header is strictly prohibited without        |
+| written permission from the original author(s).        |
++--------------------------------------------------------*/
 
 require_once 'CiviTest/CiviUnitTestCase.php';
 
@@ -447,7 +460,7 @@ class CRM_sepa_BatchingTest extends CiviUnitTestCase {
     $this->assertDBCompareValues("CRM_Contribute_DAO_ContributionRecur", array("id" => 1), $searchParams);
   }
 
-  public function _testMultipleCreditors() {
+  public function testMultipleCreditors() {
     // create a contact
     $secondCreditorId = $this->individualCreate();
     // create a creditor
