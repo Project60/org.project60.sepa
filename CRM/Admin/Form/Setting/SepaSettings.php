984170864edbc5f989b77b8b6547d18cf165c2b4
nikobo <bochan@systopia.de>
2014-05-22 09:54:22 +0200

    changed settings page input types

--- CRM/Admin/Form/Setting/SepaSettings.php
+++ CRM/Admin/Form/Setting/SepaSettings.php
@@ -13,38 +13,93 @@
 | copyright header is strictly prohibited without        |
 | written permission from the original author(s).        |
 +--------------------------------------------------------*/
- 
+
 require_once 'CRM/Admin/Form/Setting.php';
 require_once 'CRM/Core/BAO/CustomField.php';
  
 class CRM_Admin_Form_Setting_SepaSettings extends CRM_Admin_Form_Setting
 {
+
     public function buildQuickForm( ) {
         CRM_Utils_System::setTitle(ts('Sepa Direct Debit - Settings'));
  
- 
+ 		$config = CRM_Core_Config::singleton();	
+ 		print_r($config->sepasettings_ooff_horizon_days);
+ 		$ret = CRM_Core_BAO_Setting::getItem('sepasettings', "sepasettings_ooff_horizon_days");
+ 		print_r($ret);
+
         $customFields = CRM_Core_BAO_CustomField::getFields();
         $cf = array();
         foreach ($customFields as $k => $v) {
             $cf[$k] = $v['label'];
         }
 
-        $thirty_days_arr = array();
-        $thirty_days_arr[''] = ts('- select -');
-        for ($i=0; $i <= 30; $i++) { 
-        	$thirty_days_arr[$i] = $i;
-        }
- 
-        $this->addElement('select',
-                          'sepasettings_ooff_horizon_days',
-                          ts('OOFF horizon days'),
-                          $thirty_days_arr + $cf);
- 
+ 		    // OOFF
         $this->addElement('text',
-                          'sepasettings_recipient',
-                          ts('Email address to receive module notices')
+		                        'sepasettings_ooff_horizon_days',
+		                        ts('OOFF horizon days')
+		                        );
+        $this->addElement('text',
+                          'sepasettings_ooff_notice_days',
+                          ts('OOFF notice days')
+                          );
+     		// RCUR
+     		$this->addElement('text',
+                          'sepasettings_rcur_horizon_days',
+                          ts('RCUR horizon days')
                           );
  
+        $this->addElement('text',
+                          'sepasettings_rcur_notice_days',
+                          ts('RCUR notice days')
+                          );
+
+        // FRST
+ 		    $this->addElement('text',
+                          'sepasettings_frst_horizon_days',
+                          ts('FRST horizon days')
+                          );
+ 
+        $this->addElement('text',
+                          'sepasettings_frst_notice_days',
+                          ts('FRST notice days')
+                          );
+
+        // System
+        $this->addElement('text',
+                          'sepasettings_update_lock_timeout',
+                          ts('Update lock timeout')
+                          );
+
+        // Rules
+        $this->addRule('sepasettings_ooff_horizon_days', 
+                       ts('Please enter the horizon as number of days (integers only).'),
+                      'positiveInteger');
+
+        $this->addRule('sepasettings_ooff_notice_days', 
+                       ts('Please enter the notice days as number of days (integers only).'),
+                      'positiveInteger');
+
+        $this->addRule('sepasettings_rcur_horizon_days', 
+                       ts('Please enter the horizon as number of days (integers only).'),
+                      'positiveInteger');
+
+        $this->addRule('sepasettings_rcur_notice_days', 
+                       ts('Please enter the notice days as number of days (integers only).'),
+                      'positiveInteger');
+
+        $this->addRule('sepasettings_frst_horizon_days', 
+                       ts('Please enter the horizon as number of days (integers only).'),
+                      'positiveInteger');
+
+        $this->addRule('sepasettings_frst_notice_days', 
+                       ts('Please enter the notice days as number of days (integers only).'),
+                      'positiveInteger');
+
+        $this->addRule('sepasettings_update_lock_timeout', 
+                       ts('Please enter the lock timeout as number of days (integers only).'),
+                      'positiveInteger');
+
         parent::buildQuickForm();
     }
 }
--- templates/CRM/Admin/Form/Setting/SepaSettings.tpl
+++ templates/CRM/Admin/Form/Setting/SepaSettings.tpl
@@ -1,7 +1,7 @@
 <div class="crm-block crm-form-block crm-sepasettings-form-block">
   <div class="crm-submit-buttons">{include file="CRM/common/formButtons.tpl" location="top"}</div>
-<h3>Default Settings</h3>
 <fieldset>
+    <h3>Default Settings</h3>
     <table class="form-layout">
         <tr class="crm-sepasetting-form-block-ooff-horizon-days">
           <td class="label">{$form.sepasettings_ooff_horizon_days.label}</td>
@@ -9,14 +9,46 @@
             {$form.sepasettings_ooff_horizon_days.html}
           </td>
         </tr>
-         <tr class="crm-sepasettings-form-block-recipient">
-          <td class="label">{$form.sepasettings_recipient.label}</td>
+        <tr class="crm-sepasettings-form-block-ooff-notice-days">
+          <td class="label">{$form.sepasettings_ooff_notice_days.label}</td>
           <td>
-            {$form.sepasettings_recipient.html}
+            {$form.sepasettings_ooff_notice_days.html}
+          </td>
+        </tr>
+        <tr class="crm-sepasetting-form-block-rcur-horizon-days">
+          <td class="label">{$form.sepasettings_rcur_horizon_days.label}</td>
+          <td>
+            {$form.sepasettings_rcur_horizon_days.html}
+          </td>
+        </tr>
+        <tr class="crm-sepasettings-form-block-rcur-notice-days">
+          <td class="label">{$form.sepasettings_rcur_notice_days.label}</td>
+          <td>
+            {$form.sepasettings_rcur_notice_days.html}
+          </td>
+        </tr>
+        <tr class="crm-sepasetting-form-block-frst-horizon-days">
+          <td class="label">{$form.sepasettings_frst_horizon_days.label}</td>
+          <td>
+            {$form.sepasettings_frst_horizon_days.html}
+          </td>
+        </tr>
+        <tr class="crm-sepasettings-form-block-frst-notice-days">
+          <td class="label">{$form.sepasettings_frst_notice_days.label}</td>
+          <td>
+            {$form.sepasettings_frst_notice_days.html}
           </td>
         </tr>
    </table>
- 
+   <h3>System Settings</h3>
+    <table class="form-layout">
+        <tr class="crm-sepasetting-form-block-update-lock_timeout">
+          <td class="label">{$form.sepasettings_update_lock_timeout.label}</td>
+          <td>
+            {$form.sepasettings_update_lock_timeout.html}
+          </td>
+        </tr>
+   </table>
     <div class="crm-submit-buttons">{include file="CRM/common/formButtons.tpl" location="bottom"}</div>
 </fieldset>
  
