63103ff82f4acb3e75e2b9557eeaa1a65a45b7a9
Olaf Buddenhagen <antrik@digitalcourage.de>
2014-07-17 09:59:51 +0200

    WIP: Disable PHP timeouts for long-running actions
    Generating upcoming recurring payments, as well as batching / batch
    status change actions, can take quite a long time -- especially on slow
    shared servers. Thus we try to disable the PHP timeout before starting
    these actions, so they get a better chance of completing...

--- CRM/Sepa/Page/CreateNext.php
+++ CRM/Sepa/Page/CreateNext.php
@@ -3,6 +3,8 @@ require_once 'CRM/Core/Page.php';
 
 class CRM_Sepa_Page_CreateNext extends CRM_Core_Page {
   function run() {
+    set_time_limit(0); /* This action can take quite long... */
+
     #$creditorId = CRM_Utils_Request::retrieve('creditor_id', 'Positive', $_ = null, true);
     #CRM_Utils_System::setTitle("Create upcoming Recurring Contribution installments for Creditor $creditorId");
     #
