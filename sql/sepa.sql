ae4530e35d5658bf49f230ee28604f88affb7b3e
Olaf Buddenhagen <antrik@digitalcourage.de>
2015-06-18 10:11:49 +0200

    sepa.sql: Drop duplicate `civicrm_sdd_mandate`

--- sql/sepa.sql
+++ sql/sepa.sql
@@ -70,39 +70,6 @@ CREATE TABLE IF NOT EXISTS `civicrm_sdd_mandate` (
 ,          CONSTRAINT FK_civicrm_sdd_mandate_creditor_id FOREIGN KEY (`creditor_id`) REFERENCES `civicrm_sdd_creditor`(`id`) ON DELETE SET NULL,          CONSTRAINT FK_civicrm_sdd_mandate_contact_id FOREIGN KEY (`contact_id`) REFERENCES `civicrm_contact`(`id`) ON DELETE SET NULL,          CONSTRAINT FK_civicrm_sdd_mandate_first_contribution_id FOREIGN KEY (`first_contribution_id`) REFERENCES `civicrm_contribution`(`id`)   
 )  ENGINE=InnoDB DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci  ;
 
-CREATE TABLE IF NOT EXISTS `civicrm_sdd_mandate` (
-     `id` int unsigned NOT NULL AUTO_INCREMENT  COMMENT 'ID',
-     `reference` varchar(35) NOT NULL   COMMENT 'The unique mandate reference',
-     `source` varchar(64)    COMMENT 'Needed or coming from ContributionRecur? phoning/online/face 2 face....',
-     `entity_table` varchar(64)    COMMENT 'physical tablename for entity being joined, eg contributionRecur or Membership',
-     `entity_id` int unsigned NOT NULL   COMMENT 'FK to entity table specified in entity_table column.',
-     `date` datetime NOT NULL  COMMENT 'by default now()',
-     `creditor_id` int unsigned    COMMENT 'FK to ssd_creditor',
-     `contact_id` int unsigned    COMMENT 'FK to Contact ID that owns that account',
-     `iban` varchar(42) NULL   COMMENT 'Iban of the debtor',
-     `bic` varchar(11)    COMMENT 'BIC of the debtor',
-     `type` varchar(1) NOT NULL  DEFAULT 'R' COMMENT 'R for recurrent (default) O for one-shot',
-     `is_enabled` tinyint NOT NULL  DEFAULT 1 COMMENT 'If the mandate has been validated',
-     `creation_date` datetime    ,
-     `validation_date` datetime     
-,
-    PRIMARY KEY ( `id` )
- 
-    ,     UNIQUE INDEX `reference`(
-        reference
-  )
-  ,     INDEX `index_entity`(
-        entity_table
-      , entity_id
-  )
-  ,     INDEX `iban`(
-        iban
-  )
-  
-,          CONSTRAINT FK_civicrm_sdd_mandate_creditor_id FOREIGN KEY (`creditor_id`) REFERENCES `civicrm_sdd_creditor`(`id`) ON DELETE SET NULL,          CONSTRAINT FK_civicrm_sdd_mandate_contact_id FOREIGN KEY (`contact_id`) REFERENCES `civicrm_contact`(`id`) ON DELETE SET NULL  
-)  ENGINE=InnoDB DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci  ;
-
-
 -- /*******************************************************
 -- *
 -- * civicrm_sdd_file
