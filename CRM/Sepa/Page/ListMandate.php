9c1fefc5f066cb94068569b8d39d104ab70d789a
scardinius <scardinius@chords.pl>
2015-07-20 16:36:55 +0200

    List of mandates

---
+++ CRM/Sepa/Page/ListMandate.php
@@ -0,0 +1,26 @@
+<?php
+/*-------------------------------------------------------+
+| Project 60 - SEPA direct debit                         |
+| Copyright (C) 2013-2014 TTTP                           |
+| Author: X+                                             |
++--------------------------------------------------------+
+| This program is released as free software under the    |
+| Affero GPL license. You can redistribute it and/or     |
+| modify it under the terms of this license which you    |
+| can read by viewing the included agpl.txt or online    |
+| at www.gnu.org/licenses/agpl.html. Removal of this     |
+| copyright header is strictly prohibited without        |
+| written permission from the original author(s).        |
++--------------------------------------------------------*/
+
+
+require_once 'CRM/Core/Page.php';
+
+class CRM_Sepa_Page_ListMandate extends CRM_Core_Page {
+                        
+  function run() {
+
+    parent::run();
+  }
+
+}
---
+++ js/ListMandate.js
@@ -0,0 +1,19 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+CRM.$(function ($) {
+
+    // dictionary for field status (in the future):
+    /*$('[name=field_status]').crmEntityRef({
+        entity: 'contact_type',
+        select: {minimumInputLength:0},
+        api: {
+            params: {}
+        },
+        create: false
+    });*/
+
+});
