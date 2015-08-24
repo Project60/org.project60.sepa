049e1fcf0b644a1dd774811fd259044a563511bc
scardinius <scardinius@chords.pl>
2015-08-24 10:54:42 +0200

    DAO, BAO and API for mandate file and file rows

---
+++ CRM/Sepa/BAO/SEPAMandateFile.php
@@ -0,0 +1,5 @@
+<?php
+
+class CRM_Sepa_BAO_SEPAMandateFile extends CRM_Sepa_DAO_SEPAMandateFile {
+
+}
---
+++ CRM/Sepa/BAO/SEPAMandateFileRow.php
@@ -0,0 +1,5 @@
+<?php
+
+class CRM_Sepa_BAO_SEPAMandateFileRow extends CRM_Sepa_DAO_SEPAMandateFileRow {
+
+}
