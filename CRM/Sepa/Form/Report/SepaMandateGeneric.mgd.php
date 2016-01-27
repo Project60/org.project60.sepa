b808412925996dadefaa30a64a75b39f443e9d36
systopia <endres@systopia.de>
2016-01-27 08:52:16 +0100

    #358 started with boilerplate code

---
+++ CRM/Sepa/Form/Report/SepaMandateGeneric.mgd.php
@@ -0,0 +1,21 @@
+<?php
+// This file declares a managed database record of type "ReportTemplate".
+// The record will be automatically inserted, updated, or deleted from the
+// database as appropriate. For more details, see "hook_civicrm_managed" at:
+// http://wiki.civicrm.org/confluence/display/CRMDOC42/Hook+Reference
+return array (
+  0 => 
+  array (
+    'name' => 'CRM_Sepa_Form_Report_SepaMandateGeneric',
+    'entity' => 'ReportTemplate',
+    'params' => 
+    array (
+      'version' => 3,
+      'label' => 'SepaMandateGeneric',
+      'description' => 'SepaMandateGeneric (org.project60.sepa)',
+      'class_name' => 'CRM_Sepa_Form_Report_SepaMandateGeneric',
+      'report_url' => 'org.project60.sepa/sepamandategeneric',
+      'component' => 'CiviContribute',
+    ),
+  ),
+);
