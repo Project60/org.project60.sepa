026ca3bd2fc0a5f194de85735ca4dcecdf895e0e
scardinius <scardinius@chords.pl>
2016-02-18 11:30:46 +0100

    Fix upgrader revision to 1.3.0 #371

--- CRM/Sepa/Upgrader.php
+++ CRM/Sepa/Upgrader.php
@@ -11,9 +11,9 @@ class CRM_Sepa_Upgrader extends CRM_Sepa_Upgrader_Base {
    * @return TRUE on success
    * @throws Exception
    */
-  public function upgrade_0010() {
-    $this->ctx->log->info('Applying upgrade 1.0 (default currency for creditor)');
-    $this->executeSqlFile('sql/upgrade_0010.sql');
+  public function upgrade_0130() {
+    $this->ctx->log->info('Applying upgrade to 1.3.0 (default currency for creditor)');
+    $this->executeSqlFile('sql/upgrade_0130.sql');
     return TRUE;
   }
 }
