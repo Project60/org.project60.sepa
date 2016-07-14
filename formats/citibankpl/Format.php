4262f4a21b1ef487274b64d481fad9d3772eb1bf
scardinius <scardinius@chords.pl>
2016-07-14 14:29:56 +0200

    Use only accepted or active mandates during batching CitiBank format

--- formats/citibankpl/Format.php
+++ formats/citibankpl/Format.php
@@ -4,6 +4,9 @@ class CRM_Sepa_Logic_Format_citibankpl extends CRM_Sepa_Logic_Format {
 
   public static $out_charset = 'WINDOWS-1250';
 
+  /** @var string Only accepted (3) or active (5) mandates should be processed */
+  public static $generatexml_sql_where = ' AND mandate.bank_status IN (3, 5)';
+
   public function improveContent($content) {
     return preg_replace('~(*BSR_ANYCRLF)\R~', "\r\n", $content);
   }
