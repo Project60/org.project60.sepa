14c27c28288f90ca4f833d407a60c0e7f4bed161
Daniël van Vuuren <9696905+DanielvV@users.noreply.github.com>
2017-11-03 17:39:17 +0100

    Add custom tokens the new way
    https://docs.civicrm.org/dev/en/latest/framework/civimail/#tokens

--- sepa.php
+++ sepa.php
@@ -683,3 +683,42 @@ function sepa_civicrm_tokenValues(&$values, $cids, $job = null, $tokens = array(
     }
   }
 }
+
+function sepa_civicrm_container(&$container) {
+  $container->addResource(new \Symfony\Component\Config\Resource\FileResource(__FILE__));
+  $container->findDefinition('dispatcher')->addMethodCall('addListener',
+    array(\Civi\Token\Events::TOKEN_REGISTER, 'sepa_register_tokens')
+  );
+  $container->findDefinition('dispatcher')->addMethodCall('addListener',
+    array(\Civi\Token\Events::TOKEN_EVALUATE, 'sepa_evaluate_tokens')
+  );
+}
+
+function sepa_register_tokens(\Civi\Token\Event\TokenRegisterEvent $e) {
+  $e->entity('sepa')
+    ->register('frequencyUnit', ts('Frequency Unit'))
+    ->register('frequencyInterval', ts('Frequency Interval'))
+    ->register('financialTypeId', ts('Financial Type Id'));
+}
+
+function sepa_evaluate_tokens(\Civi\Token\Event\TokenValueEvent $e) {
+  foreach ($e->getRows() as $row) {
+    if ($row->context['entity_table'] == "civicrm_contribution_recur") {
+      $result = civicrm_api3('ContributionRecur', 'get', array(
+        'return' => array("frequency_unit", "frequency_interval", "financial_type_id"),
+        'id' => $row->context['entity_id'],
+      ));
+    } else {
+      $result = civicrm_api3('Contribution', 'get', array(
+        'return' => array("financial_type_id"),
+        'id' => $row->context['entity_id'],
+      ));
+    }
+
+    /** @var TokenRow $row */
+    $row->format('text/html');
+    $row->tokens('sepa', 'frequencyUnit', $result['values'][$result['id']]['frequency_unit']);
+    $row->tokens('sepa', 'frequencyInterval', $result['values'][$result['id']]['frequency_interval']);
+    $row->tokens('sepa', 'financialTypeId', $result['values'][$result['id']]['financial_type_id']);
+  }
+}
