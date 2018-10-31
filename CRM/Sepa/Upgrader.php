85d39e5308ade02a81469967554972628974f590
B. Endres <endres@systopia.de>
2018-10-31 16:43:37 +0100

    added upgrader for #514

--- CRM/Sepa/Upgrader.php
+++ CRM/Sepa/Upgrader.php
@@ -135,5 +135,42 @@ class CRM_Sepa_Upgrader extends CRM_Sepa_Upgrader_Base {
     $customData->syncOptionGroup(E::path('resources/formats_option_group.json'));
     return TRUE;
   }
+
+  /**
+   * Fixes the damages caused by SEPA-514
+   *
+   * @return TRUE on success
+   * @throws Exception
+   */
+  public function upgrade_1412() {
+    $this->ctx->log->info('Applying update 1412');
+    // set all SEPA recurring contributions in status 'In Progress' to 'Pending'
+    $status_pending    = (int) CRM_Core_PseudoConstant::getKey('CRM_Contribute_BAO_Contribution', 'contribution_status_id', 'Pending');
+    $status_inprogress = (int) CRM_Core_PseudoConstant::getKey('CRM_Contribute_BAO_Contribution', 'contribution_status_id', 'In Progress');
+    CRM_Core_DAO::executeQuery("
+        UPDATE civicrm_contribution_recur rcur
+        LEFT JOIN civicrm_sdd_mandate  mandate ON mandate.entity_id = rcur.id 
+                                               AND mandate.entity_table = 'civicrm_contribution_recur'
+          SET rcur.contribution_status_id = {$status_pending}
+        WHERE rcur.contribution_status_id = {$status_inprogress}
+          AND mandate.id IS NOT NULL;");
+
+    // count number of loose 'In Progress' SEPA contributions,
+    //  i.e. the ones that are not in any batch group
+    $lost_contributions = CRM_Core_DAO::singleValueQuery("
+        SELECT COUNT(*)
+        FROM civicrm_contribution contribution
+        LEFT JOIN  civicrm_sdd_contribution_txgroup c2txg ON c2txg.contribution_id = contribution.id 
+        LEFT JOIN civicrm_sdd_mandate  mandate ON mandate.entity_id = contribution.contribution_recur_id 
+                                               AND mandate.entity_table = 'civicrm_contribution_recur'
+        WHERE contribution.contribution_status_id = {$status_inprogress}
+          AND mandate.id IS NOT NULL
+          AND c2txg.id IS NULL;");
+    if ($lost_contributions) {
+      CRM_Core_Session::setStatus("There seems to be {$lost_contributions} SEPA contributions in status 'In Progress', that are not in any transaction group. This is likely due to the bug SEPA-514, and you might want to check, if these shouldn't be deleted.");
+    }
+
+    return TRUE;
+  }
 }
 
