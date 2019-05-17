d7d89451a1585436d09ae2690d6268c7fdbbb5f8
B. Endres <endres@systopia.de>
2019-05-17 15:39:49 +0200

    [#534] moving stuff to org.project60.sepapp

--- CRM/Sepa/Upgrader.php
+++ CRM/Sepa/Upgrader.php
@@ -174,19 +174,6 @@ class CRM_Sepa_Upgrader extends CRM_Sepa_Upgrader_Base {
   }
 
   /**
-   * Make sure the new PP is available
-   *
-   * @return TRUE on success
-   * @throws Exception
-   */
-  public function upgrade_1413() {
-    $this->ctx->log->info('Applying update 1413');
-    // make sure the new payment processor is available
-    sepa_pp_enable();
-    return TRUE;
-  }
-
-  /**
    * Make sure, the payment_processor_id in civicrm_sdd_creditor is NULL.
    *  This *shouldn't* be the set, but if it is, it can cause errors.
    *
