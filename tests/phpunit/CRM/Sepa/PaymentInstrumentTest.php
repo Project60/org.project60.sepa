3d2586df75bae9388e6d2f07e32e5d9edcac6d8f
B. Endres <endres@systopia.de>
2020-07-31 11:08:04 -0500

    [#572] adding more tests

--- tests/phpunit/CRM/Sepa/PaymentInstrumentTest.php
+++ tests/phpunit/CRM/Sepa/PaymentInstrumentTest.php
@@ -21,11 +21,12 @@ use CRM_Sepa_ExtensionUtil as E;
  *
  * Testing:
  *  payment instruments in a traditional SEPA mandate live cycle (OOFF / RCUR)
- *  payment instruments with customised payemnt instruments
+ *  creditors with customised payment instruments
  *    - unique payment instruments
  *    - multiple payment instruments
  *    - no ooff (disabled)
  *    - no rcur (disabled)
+ *  custom frst/rcur cycles
  *
  * @group headless
  */
@@ -126,4 +127,148 @@ class CRM_Sepa_PaymentInstrumentTest extends CRM_Sepa_TestBase
     $contribution = $this->getLatestContributionForMandate($mandate);
     $this->assertEquals(1, $contribution['payment_instrument_id'], "Multi-PI creditor's mandate should set the passed payment instrument (1)");
   }
+
+  /**
+   * Test the RCUR for multi-payment-instruments creditor
+   */
+  public function testMultiPIMandateRCUR()
+  {
+    $custom_creditor_id = $this->getCustomCreditor(['pi_rcur' => '1,2,3']);
+
+    // test 1: you shouldn't be able to create a mandate without PI
+    try {
+      $mandate = $this->createMandate(
+        [
+          'type'        => self::MANDATE_TYPE_RCUR,
+          'creditor_id' => $custom_creditor_id,
+        ]
+      );
+      $this->assertFalse(true, "Trying to create a multi-PI creditor's mandate without a payment instrument should thrown an exception");
+    } catch (Exception $ex) {
+      $this->assertTrue(true, "This worked as expected");
+    }
+
+
+    // test 2: you not able to create a mandate with the wrong PI
+    try {
+      $mandate = $this->createMandate(
+        [
+          'type'        => self::MANDATE_TYPE_RCUR,
+          'creditor_id' => $custom_creditor_id,
+          'payment_instrument_id' => 5,
+        ]
+      );
+      $this->assertFalse(true, "Trying to create a multi-PI creditor's mandate a wrong payment instrument should thrown an exception");
+    } catch (Exception $ex) {
+      $this->assertTrue(true, "This worked as expected");
+    }
+
+
+    // test 3: you should be able to create a mandate with PI
+    $mandate = $this->createMandate(
+      [
+        'type'                  => self::MANDATE_TYPE_RCUR,
+        'creditor_id'           => $custom_creditor_id,
+        'payment_instrument_id' => 1
+      ]
+    );
+    $this->assertNotEmpty($mandate['id'], "Creating a multi-PI creditor's mandate with a payment instrument should have worked");
+
+    // NOW: do some testing
+    $this->executeBatching(self::MANDATE_TYPE_FRST);
+    $mandate = $this->getMandate($mandate['id']);
+    $contribution = $this->getLatestContributionForMandate($mandate);
+    $this->assertEquals(1, $contribution['payment_instrument_id'], "Requested payment instrument [1] not set.");
+
+    // close & check
+    $transactionGroup = $this->getActiveTransactionGroup(self::MANDATE_TYPE_FRST);
+    $this->closeTransactionGroup($transactionGroup['id']);
+    $this->executeBatching(self::MANDATE_TYPE_RCUR, "now + 1 month");
+    $mandate = $this->getMandate($mandate['id']);
+    $contribution = $this->getLatestContributionForMandate($mandate);
+    $this->assertEquals(1, $contribution['payment_instrument_id'], "Requested payment instrument [1] not set in second installment.");
+  }
+
+  /**
+   * Test if disabling OOFF works (for new mandates)
+   */
+  public function testDisabledOOFF()
+  {
+    $custom_creditor_id = $this->getCustomCreditor(['pi_ooff' => '']);
+
+    // test 1: you shouldn't be able to create a mandate without PI
+    try {
+      $mandate = $this->createMandate(
+        [
+          'type'        => self::MANDATE_TYPE_OOFF,
+          'creditor_id' => $custom_creditor_id,
+        ]
+      );
+      $this->assertFalse(
+        true,
+        "Trying to create a OOFF mandate when those payment instruments are disabled should have thrown an exeption"
+      );
+    } catch (Exception $ex) {
+      $this->assertTrue(true, "This worked as expected");
+    }
+
+    // test 1: you shouldn't be able to create a mandate with PI
+    try {
+      $mandate = $this->createMandate(
+        [
+          'type'                  => self::MANDATE_TYPE_OOFF,
+          'creditor_id'           => $custom_creditor_id,
+          'payment_instrument_id' => 5,
+        ]
+      );
+      $this->assertFalse(
+        true,
+        "Trying to create a OOFF mandate when those payment instruments are disabled should have thrown an exeption"
+      );
+    } catch (Exception $ex) {
+      $this->assertTrue(true, "This worked as expected");
+    }
+  }
+
+  /**
+   * Test if disabling RCUR works (for new mandates)
+   */
+  public function testDisabledRCUR()
+  {
+    $custom_creditor_id = $this->getCustomCreditor(['pi_rcur' => '']);
+
+    // test 1: you shouldn't be able to create a mandate without PI
+    try {
+      $mandate = $this->createMandate(
+        [
+          'type'        => self::MANDATE_TYPE_RCUR,
+          'creditor_id' => $custom_creditor_id,
+        ]
+      );
+      $this->assertFalse(
+        true,
+        "Trying to create a RCUR mandate when those payment instruments are disabled should have thrown an exeption"
+      );
+    } catch (Exception $ex) {
+      $this->assertTrue(true, "This worked as expected");
+    }
+
+    // test 1: you shouldn't be able to create a mandate with PI
+    try {
+      $mandate = $this->createMandate(
+        [
+          'type'                  => self::MANDATE_TYPE_RCUR,
+          'creditor_id'           => $custom_creditor_id,
+          'payment_instrument_id' => 5,
+        ]
+      );
+      $this->assertFalse(
+        true,
+        "Trying to create a RCUR mandate when those payment instruments are disabled should have thrown an exeption"
+      );
+    } catch (Exception $ex) {
+      $this->assertTrue(true, "This worked as expected");
+    }
+  }
+
 }
