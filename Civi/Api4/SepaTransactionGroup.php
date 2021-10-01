d0a7854fb7050f886297280756eec1352f7db2ca
B. Endres <endres@systopia.de>
2021-10-01 09:41:52 +0200

    added apiv4

---
+++ Civi/Api4/SepaTransactionGroup.php
@@ -0,0 +1,13 @@
+<?php
+namespace Civi\Api4;
+
+/**
+ * Resource entity.
+ *
+ * Provided by the CiviCRM Resource Management extension.
+ *
+ * @package Civi\Api4
+ */
+class SepaTransactionGroup extends Generic\DAOEntity {
+
+}
