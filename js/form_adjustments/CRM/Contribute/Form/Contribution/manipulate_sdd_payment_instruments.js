06531964c4e778a2861c384b2f34009af8ae0396
B. Endres <endres@systopia.de>
2021-10-26 12:35:10 +0200

    [#572] fixed PI restriction

--- js/form_adjustments/CRM/Contribute/Form/Contribution/manipulate_sdd_payment_instruments.js
+++ js/form_adjustments/CRM/Contribute/Form/Contribution/manipulate_sdd_payment_instruments.js
@@ -16,8 +16,7 @@ cj(document).ready(function() {
   // make sure we only keep the ones we want
   if (typeof CRM.vars.sdd.pis_keep !== 'undefined' && CRM.vars.sdd.pis_keep !== null) {
     cj("select[name=payment_instrument_id] option").each(function() {
-
-      var pi = cj(this).attr('value');
+      let pi = parseInt(cj(this).attr('value'));
       if (cj.inArray(pi, CRM.vars.sdd.pis_keep) == -1) {
         cj(this).remove();
       }
@@ -26,7 +25,7 @@ cj(document).ready(function() {
 
   // make sure drop the ones we don't want
   if (typeof CRM.vars.sdd.pis_remove !== 'undefined' && CRM.vars.sdd.pis_remove !== null) {
-    for (var i = 0; i < CRM.vars.sdd.pis_remove.length; i++) {
+    for (let i = 0; i < CRM.vars.sdd.pis_remove.length; i++) {
       cj("select[name=payment_instrument_id] option[value=" + CRM.vars.sdd.pis_remove[i] + "]").remove();
     }
   }
