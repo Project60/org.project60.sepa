546c71ab5ef643495a19142ea1ae6881bd2efcbb
B. Endres <endres@systopia.de>
2023-05-04 21:08:56 +0200

    [#669] added EBICS3.6 format

---
+++ templates/Sepa/Formats/pain_008_001_02_ebics36/Format.php
@@ -0,0 +1,17 @@
+<?php
+/*-------------------------------------------------------+
+| Project 60 - SEPA direct debit                         |
+| Copyright (C) 2023-2023                                |
++--------------------------------------------------------+
+| This program is released as free software under the    |
+| Affero GPL license. You can redistribute it and/or     |
+| modify it under the terms of this license which you    |
+| can read by viewing the included agpl.txt or online    |
+| at www.gnu.org/licenses/agpl.html. Removal of this     |
+| copyright header is strictly prohibited without        |
+| written permission from the original author(s).        |
++--------------------------------------------------------*/
+
+class CRM_Sepa_Logic_Format_pain_008_001_02_ebics36 extends CRM_Sepa_Logic_Format {
+
+}
