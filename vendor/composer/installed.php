6fdaf4b112236968aa247332ac1977d30b2cf4a3
Jens Schuppe <schuppe@systopia.de>
2026-06-26 07:55:39 +0200

    Set version to 1.14.0-alpha1

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'project60/org.project60.sepa',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'd543a366df062bb429fdf3c2a854e46f428bf62b',
+        'type' => 'civicrm-ext',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'project60/org.project60.sepa' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'd543a366df062bb429fdf3c2a854e46f428bf62b',
+            'type' => 'civicrm-ext',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'webmozart/assert' => array(
+            'pretty_version' => '1.12.1',
+            'version' => '1.12.1.0',
+            'reference' => '9be6926d8b485f55b9229203f962b51ed377ba68',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../webmozart/assert',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
