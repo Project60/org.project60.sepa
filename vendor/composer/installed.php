6068569b4b30a279fc1b9ff03690e0647720821d
Dominic Tubach <tubach@systopia.de>
2026-07-15 16:42:38 +0200

    Set version to 1.14.0-beta3

---
+++ vendor/composer/installed.php
@@ -0,0 +1,36 @@
+<?php return array(
+    'root' => array(
+        'name' => 'project60/org.project60.sepa',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'd8a047688a1b1c59b48c7390af84bd2dcf027baf',
+        'type' => 'civicrm-ext',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(
+            0 => '1.14.x-dev',
+        ),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'project60/org.project60.sepa' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'd8a047688a1b1c59b48c7390af84bd2dcf027baf',
+            'type' => 'civicrm-ext',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(
+                0 => '1.14.x-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+        'webmozart/assert' => array(
+            'pretty_version' => '1.12.1',
+            'version' => '1.12.1.0',
+            'reference' => '9be6926d8b485f55b9229203f962b51ed377ba68',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../webmozart/assert',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
