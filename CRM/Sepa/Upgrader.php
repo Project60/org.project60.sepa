7f5cb929939b556b948900de0a9da943600e13cd
Dominic Tubach <tubach@systopia.de>
2026-08-12 10:55:16 +0200

    Fix upgrade_11304
    Index `mandate_id` on `civicrm_sdd_entity_mandate` cannot be deleted
    because it's used by a FK constraint.

--- CRM/Sepa/Upgrader.php
+++ CRM/Sepa/Upgrader.php
@@ -583,10 +583,6 @@ class CRM_Sepa_Upgrader extends CRM_Extension_Upgrader_Base {
   }
 
   public function upgrade_11304(): bool {
-    $this->ctx->log->info('Delete database index mandate_id ON civicrm_sdd_entity_mandate');
-    // There is a foreign key constraint so there's no need for an additional index.
-    $this->executeSql('DROP INDEX IF EXISTS mandate_id ON civicrm_sdd_entity_mandate');
-
     return TRUE;
   }
 
